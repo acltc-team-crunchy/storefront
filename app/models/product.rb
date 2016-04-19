b6f3e93702621f7c146b63a49edde16b8aeea297
Mark Richardson <markeban@yahoo.com>
2016-04-18 20:27:21 -0700

    finished discount, tax, total methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ActiveRecord::Base
+
+  DISCOUNT_THRESHOLD = 50
+  SALES_TAX = 0.09
+
+  def sale_message
+    if price < DISCOUNT_THRESHOLD
+      "Discount Item!"
+    else
+      "On Sale!"
+    end
+  end
+
+  def tax
+    price * SALES_TAX
+  end
+
+  def total
+    price + tax
+  end
 end
