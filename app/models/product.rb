e945331c099f3c9b3e09e36b2913ebf891f155ed
Mark Richardson <markeban@yahoo.com>
2016-05-08 10:15:39 -0700

    added bootstrap through vendor

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ActiveRecord::Base
   has_many :orders, through: :carted_products
 
   validates :name, :description, :price, presence: true
-  validates :price, numericality: {only_integer: true, greater_than: 0}
+  # validates :price, numericality: {only_integer: true, greater_than: 0}
   validates :name, uniqueness: true
 
   DISCOUNT_THRESHOLD = 50
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
-
+User.create!([
+  {email: "mark@anyonecanlearntoccode.com", password: "password"} 
+])
+CartedProduct.create!([
+  {product_id: 1, order_id: 1, quantity: 1},
+  {product_id: 2, order_id: 1, quantity: 3},
+  {product_id: 3, order_id: 1, quantity: 1},
+  {product_id: 1, order_id: 2, quantity: 3},
+  {product_id: 3, order_id: 2, quantity: 2},
+  {product_id: 4, order_id: 2, quantity: 1}
+])
 Image.create!([
   {product_id: 1, image_url: "http://www.foodpeoplewant.com/wp-content/uploads/2009/09/Tacos-De-Cochinita-Pibil-1024x686.jpg"},
   {product_id: 2, image_url: "http://upload.wikimedia.org/wikipedia/commons/b/b5/01_Tacos_al_Pastor.jpg"},
@@ -9,13 +19,15 @@ Image.create!([
   {product_id: 1, image_url: "https://d29vij1s2h2tll.cloudfront.net/~/media/images/taco-bell/products/heroes/22848_specialties_quesalupa_1400x800.jpg"},
   {product_id: 1, image_url: "http://www.beavermethodist.org/wp-content/uploads/tacooo-copy.jpg"}
 ])
+
 Product.create!([
-  {name: "Cochinita Pibil", price: "126.0", description: "Mouthwatering, slow-cooked pork on a corn tortilla, served with queso fresco, pickled onions y cilantro!", rating: nil, status: true, supplier_id: 1},
-  {name: "Al Pastor", price: "14.0", description: "Succulent, smoky, earthy, spiced and grilled pork strips seasoned with adobo seasoning. Served on a corn tortilla with cilantro y cebollas", rating: nil, status: true, supplier_id: 1},
-  {name: "Tacos de Asador", price: "343.0", description: "Skirt steak seasoned and grilled until crisp, served in a corn tortilla with cilantro y cebollas. Spicy habanero or medium chile salsa upon request. #awesome", rating: nil, status: true, supplier_id: 2},
-  {name: "Tacos de pescado", price: "121.0", description: "Fresh Mahi Mahi battered and flash-fried, served on a corn tortilla and garnished with slaw, crema and a side of avocado, lime and pico de gallo.", rating: "5", status: true, supplier_id: 2},
-  {name: "Exploding Taco", price: "10.0", description: "BOOM!", rating: nil, status: true, supplier_id: 1},
-  {name: "Carnitas", price: "65.0", description: "Twice-cooked mouthwatering pork tacos served on a corn tortilla con cebollas y cilantro", rating: nil, status: true, supplier_id: 2}
+  {name: "Cochinita Pibil", price: 126.0, description: "Mouthwatering, slow-cooked pork on a corn tortilla, served with queso fresco, pickled onions y cilantro!", rating: nil, status: true, supplier_id: 1},
+  {name: "Al Pastor", price: 14.0, description: "Succulent, smoky, earthy, spiced and grilled pork strips seasoned with adobo seasoning. Served on a corn tortilla with cilantro y cebollas", rating: nil, status: true, supplier_id: 1},
+  {name: "Tacos de Asador", price: 343.0, description: "Skirt steak seasoned and grilled until crisp, served in a corn tortilla with cilantro y cebollas. Spicy habanero or medium chile salsa upon request. #awesome", rating: nil, status: true, supplier_id: 2},
+  {name: "Tacos de pescado", price: 121.0, description: "Fresh Mahi Mahi battered and flash-fried, served on a corn tortilla and garnished with slaw, crema and a side of avocado, lime and pico de gallo.", rating: 5, status: true, supplier_id: 2},
+  {name: "Exploding Taco", price: 10.0, description: "BOOM!", rating: nil, status: true, supplier_id: 1},
+  {name: "Carnitas", price: 65.0, description: "Twice-cooked mouthwatering pork tacos served on a corn tortilla con cebollas y cilantro", rating: nil, status: true, supplier_id: 2},
+  {name: "Passable taco", price: 45.0, description: "must save to database", rating: nil, status: true, supplier_id: 2}
 ])
 Supplier.create!([
   {name: "Bob's Tacos", email: "bob@gmail.com", phone: "343-234-2342"},
